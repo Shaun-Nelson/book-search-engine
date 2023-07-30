516c70e67bb5c758216cdd0943bcf8410b9c7f54
Shaun Nelson <shaunptnelson@gmail.com>
2023-07-29 22:54:58 -0400

    Added loading state to SavedBooks page and added remove book functionality

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Container, Card, Button, Row, Col } from "react-bootstrap";
 import { useQuery, useMutation } from "@apollo/client";
 import { GET_ME } from "../utils/queries";
@@ -26,7 +26,7 @@ const SavedBooks = () => {
 
     try {
       // use removeBook mutation to remove book from user's saved list
-      const response = await removeBook({
+      await removeBook({
         variables: {
           bookId: bookId,
           username: userData.username,
@@ -42,48 +42,54 @@ const SavedBooks = () => {
 
   return (
     <>
-      <div fluid className='text-light bg-dark p-5'>
-        <Container>
-          <h1>Viewing saved books!</h1>
-        </Container>
-      </div>
-      <Container>
-        <h2 className='pt-5'>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${
-                userData.savedBooks.length === 1 ? "book" : "books"
-              }:`
-            : "You have no saved books!"}
-        </h2>
-        <Row>
-          {userData.savedBooks.map((book) => {
-            return (
-              <Col md='4'>
-                <Card key={book.bookId} border='dark'>
-                  {book.image ? (
-                    <Card.Img
-                      src={book.image}
-                      alt={`The cover for ${book.title}`}
-                      variant='top'
-                    />
-                  ) : null}
-                  <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <p className='small'>Authors: {book.authors}</p>
-                    <Card.Text>{book.description}</Card.Text>
-                    <Button
-                      className='btn-block btn-danger'
-                      onClick={() => handleDeleteBook(book.bookId)}
-                    >
-                      Delete this Book!
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <>
+          <div fluid className='text-light bg-dark p-5'>
+            <Container>
+              <h1>Viewing saved books!</h1>
+            </Container>
+          </div>
+          <Container>
+            <h2 className='pt-5'>
+              {userData.savedBooks.length
+                ? `Viewing ${userData.savedBooks.length} saved ${
+                    userData.savedBooks.length === 1 ? "book" : "books"
+                  }:`
+                : "You have no saved books!"}
+            </h2>
+            <Row>
+              {userData.savedBooks.map((book) => {
+                return (
+                  <Col md='4'>
+                    <Card key={book.bookId} border='dark'>
+                      {book.image ? (
+                        <Card.Img
+                          src={book.image}
+                          alt={`The cover for ${book.title}`}
+                          variant='top'
+                        />
+                      ) : null}
+                      <Card.Body>
+                        <Card.Title>{book.title}</Card.Title>
+                        <p className='small'>Authors: {book.authors}</p>
+                        <Card.Text>{book.description}</Card.Text>
+                        <Button
+                          className='btn-block btn-danger'
+                          onClick={() => handleDeleteBook(book.bookId)}
+                        >
+                          Delete this Book!
+                        </Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                );
+              })}
+            </Row>
+          </Container>
+        </>
+      )}
     </>
   );
 };
